d6c9eb0c74e42efc8f4a5fdd31ee61f4e417fb9c
yonraf <moraf19@student.sdu.dk>
2019-10-30 14:38:21 +0100

    Tilføjet Trash-Class etc.

---
+++ WorldOfTrashNew/src/worldofzuul/Trash.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Yonus Rafik
+ */
+
+public class Trash {
+    private String name;
+    
+    public Trash (String name)
+    {
+        this.name = name; 
+    }
+    
+    public String getName() 
+    {
+        return name; 
+    }
+}
+
