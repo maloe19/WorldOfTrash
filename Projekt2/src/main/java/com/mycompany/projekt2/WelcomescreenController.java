7baffddef5f69f607b57665404019da95202fdea
Belmin <Behod19@student.sdu.dk>
2019-12-10 21:12:55 +0100

    welcome skærm og skriv navn i GUI

--- Projekt2/src/main/java/com/mycompany/projekt2/WelcomescreenController.java
+++ Projekt2/src/main/java/com/mycompany/projekt2/WelcomescreenController.java
@@ -5,37 +5,40 @@
  */
 package com.mycompany.projekt2;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
  * @author Bruger
  */
-public class MainmenuController implements Initializable {
+public class WelcomescreenController implements Initializable {
 
 
     @FXML
-    private Button Play;
+    private TextField writename;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        System.out.println(new File(".").getAbsolutePath());
     }    
     
     @FXML
-    private void startGame(ActionEvent event) throws IOException {
-        App.setRoot("welcomescreen");
+    private void enterName(ActionEvent event) throws IOException {
+        
+        App.g.play();
+        App.g.createRooms();
+        
+        App.setRoot("outside");
     }
 
 }
